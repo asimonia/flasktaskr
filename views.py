f349369bb975eece20470b61847d73e9187ee3ea
asimonia <alex.simonian@gmail.com>
2016-05-15 22:09:02 -0500

    Added SQLAlchemy functionality

--- views.py
+++ views.py
@@ -4,6 +4,7 @@
 
 from forms import AddTaskForm, RegisterForm, LoginForm
 
+import datetime
 from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -39,6 +40,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('Goodbye!')
 	return redirect(url_for('login'))
 
@@ -52,6 +54,7 @@ def login():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash('Welcome!')
 				return redirect(url_for('tasks'))
 			else:
@@ -76,11 +79,15 @@ def new_task():
 	form = AddTaskForm(request.form)
 	if request.method == 'POST':
 		if form.validate_on_submit():
-			new_task = Task(form.name.data, form.due_date.data, form.priority.data, '1')
+			new_task = Task(form.name.data, form.due_date.data, form.priority.data, datetime.datetime.utcnow(), '1', session['user_id'])
 			db.session.add(new_task)
 			db.session.commit()
 			flash('New entry was successfully posted. Thanks.')
-		return redirect(url_for('tasks'))
+			return redirect(url_for('tasks'))
+		else:
+			flash('All fields are required.')
+			return redirect(url_for('tasks'))
+	return render_template('tasks.html', form=form)
 
 
 # Mark tasks as complete
