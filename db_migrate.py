af202e6b0bed87df00080d92eea8424af68e5718
asimonia <alex.simonian@gmail.com>
2016-05-16 23:43:38 -0500

    Completed nosetests

--- db_migrate.py
+++ db_migrate.py
@@ -4,8 +4,9 @@ from views import db
 from _config import DATABASE_PATH
 
 import sqlite3
-from datetime import datetime
+# from datetime import datetime
 
+"""
 with sqlite3.connect(DATABASE_PATH) as connection:
 
 	# get a cursor object used to execute SQL commands
@@ -29,4 +30,23 @@ with sqlite3.connect(DATABASE_PATH) as connection:
 
 	# delete old_tasks table
 	c.execute("DROP TABLE old_tasks")
+"""
 
+with sqlite3.connect(DATABASE_PATH) as connection:
+
+	c = connection.cursor()
+
+	c.execute("""ALTER TABLE users RENAME TO old_users""")
+
+	db.create_all()
+
+	c.execute("""SELECT name, email, password
+				 FROM old_users
+				 ORDER BY id ASC""")
+
+	data = [(row[0], row[1], row[2], 'user') for row in c.fetchall()]
+
+	c.executemany("""INSERT INTO users (name, email, password,
+					role) VALUES (?, ?, ?, ?)""", data)
+
+	c.execute("DROP TABLE old_users")
